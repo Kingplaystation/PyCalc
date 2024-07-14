47d5956cc7036344973f6b5c395b5bdfff5daad3
Oliullah <alamshahin465686@gmail.com>
2024-07-14 17:15:16 +0600

    Update

--- PyCalc.py
+++ PyCalc.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QMenu, QMessageBox
-from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 import math
 
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.dark_mode = False
+        self.square_buttons = False
         self.initUI()
 
     def initUI(self):
@@ -16,12 +17,15 @@ class Calculator(QWidget):
         self.update_style()
 
         layout = QVBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(10, 10, 10, 10)
 
-        # Add settings button to top left corner
         top_layout = QHBoxLayout()
+        top_layout.setContentsMargins(0, 0, 0, 10)
         self.settings_button = QPushButton()
-        self.settings_button.setIcon(QIcon('settings_icon.png'))  # Make sure to have a settings icon image file
+        self.settings_button.setIcon(QIcon('settings_icon.png'))
         self.settings_button.clicked.connect(self.show_settings_menu)
+        self.settings_button.setFixedSize(40, 40)
         self.update_settings_button_style()
         top_layout.addWidget(self.settings_button, alignment=Qt.AlignLeft)
         top_layout.addStretch()
@@ -34,110 +38,86 @@ class Calculator(QWidget):
         layout.addWidget(self.display)
 
         buttons = [
-            'C', '±', '%', '÷',
+            'C', '±', '%', '÷', 
             '7', '8', '9', '×',
             '4', '5', '6', '-',
             '1', '2', '3', '+',
-            '0', '.', '√', '='
+            '0', '.', '√', '=',
+            '⌫'
         ]
 
         grid_layout = QHBoxLayout()
+        grid_layout.setSpacing(5)
         for i in range(4):
             col_layout = QVBoxLayout()
+            col_layout.setSpacing(5)
             for j in range(5):
                 index = i + 4*j
                 if index < len(buttons):
                     button = QPushButton(buttons[index])
                     button.clicked.connect(self.on_click)
+                    button.setFixedSize(60, 60)
                     self.update_button_style(button)
                     col_layout.addWidget(button)
             grid_layout.addLayout(col_layout)
+
+        backspace_button = QPushButton('⌫')
+        backspace_button.clicked.connect(self.on_click)
+        backspace_button.setFixedSize(60, 60)
+        self.update_button_style(backspace_button)
+        grid_layout.addWidget(backspace_button)
 
         layout.addLayout(grid_layout)
 
         self.setLayout(layout)
 
     def update_style(self):
-        if self.dark_mode:
-            self.setStyleSheet("background-color: #2D2D2D;")
-        else:
-            self.setStyleSheet("background-color: #F2F2F2;")
+        self.setStyleSheet(f"background-color: {'#2D2D2D' if self.dark_mode else '#F2F2F2'};")
 
     def update_display_style(self):
-        if self.dark_mode:
-            self.display.setStyleSheet("background-color: #3D3D3D; color: #FFFFFF; font-size: 24px; padding: 10px; margin: 10px; border: none;")
-        else:
-            self.display.setStyleSheet("background-color: #FFFFFF; color: #000000; font-size: 24px; padding: 10px; margin: 10px; border: none;")
+        self.display.setStyleSheet(f"""
+            background-color: {'#3D3D3D' if self.dark_mode else '#FFFFFF'};
+            color: {'#FFFFFF' if self.dark_mode else '#000000'};
+            font-size: 24px;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: none;
+            border-radius: 5px;
+        """)
 
     def update_button_style(self, button):
-        if self.dark_mode:
-            button.setStyleSheet("""
-                QPushButton {
-                    background-color: #4D4D4D;
-                    color: #FFFFFF;
-                    font-size: 18px;
-                    border: none;
-                    padding: 15px;
-                    margin: 5px;
-                }
-                QPushButton:hover {
-                    background-color: #5D5D5D;
-                }
-                QPushButton:pressed {
-                    background-color: #6D6D6D;
-                }
-            """)
-        else:
-            button.setStyleSheet("""
-                QPushButton {
-                    background-color: #E6E6E6;
-                    color: #000000;
-                    font-size: 18px;
-                    border: none;
-                    padding: 15px;
-                    margin: 5px;
-                }
-                QPushButton:hover {
-                    background-color: #D4D4D4;
-                }
-                QPushButton:pressed {
-                    background-color: #BFBFBF;
-                }
-            """)
+        border_radius = "5px" if self.square_buttons else "30px"
+        button.setStyleSheet(f"""
+            QPushButton {{
+                background-color: {'#4D4D4D' if self.dark_mode else '#E6E6E6'};
+                color: {'#FFFFFF' if self.dark_mode else '#000000'};
+                font-size: 18px;
+                border: none;
+                border-radius: {border_radius};
+            }}
+            QPushButton:hover {{
+                background-color: {'#5D5D5D' if self.dark_mode else '#D4D4D4'};
+            }}
+            QPushButton:pressed {{
+                background-color: {'#6D6D6D' if self.dark_mode else '#BFBFBF'};
+            }}
+        """)
 
     def update_settings_button_style(self):
-        if self.dark_mode:
-            self.settings_button.setStyleSheet("""
-                QPushButton {
-                    background-color: #4D4D4D;
-                    color: white;
-                    border: none;
-                    padding: 10px;
-                    margin: 5px;
-                }
-                QPushButton:hover {
-                    background-color: #5D5D5D;
-                }
-                QPushButton:pressed {
-                    background-color: #6D6D6D;
-                }
-            """)
-        else:
-            self.settings_button.setStyleSheet("""
-                QPushButton {
-                    background-color: #E6E6E6;
-                    color: black;
-                    border: none;
-                    padding: 10px;
-                    margin: 5px;
-                }
-                QPushButton:hover {
-                    background-color: #D4D4D4;
-                }
-                QPushButton:pressed {
-                    background-color: #BFBFBF;
-                }
-            """)
+        self.settings_button.setStyleSheet(f"""
+            QPushButton {{
+                background-color: {'#4D4D4D' if self.dark_mode else '#E6E6E6'};
+                color: {'white' if self.dark_mode else 'black'};
+                border: none;
+                border-radius: 20px;
+            }}
+            QPushButton:hover {{
+                background-color: {'#5D5D5D' if self.dark_mode else '#D4D4D4'};
+            }}
+            QPushButton:pressed {{
+                background-color: {'#6D6D6D' if self.dark_mode else '#BFBFBF'};
+            }}
+        """)
 
     def on_click(self):
         button = self.sender()
@@ -145,7 +125,6 @@ class Calculator(QWidget):
 
         if button.text() == '=':
             try:
-                # Replace '×' with '*' and '÷' with '/'
                 expression = current_text.replace('×', '*').replace('÷', '/')
                 result = eval(expression)
                 self.display.setText(str(result))
@@ -156,35 +135,33 @@ class Calculator(QWidget):
         elif button.text() == '±':
             try:
                 value = float(current_text)
-                result = -value
-                self.display.setText(str(result))
+                self.display.setText(str(-value))
             except:
                 self.display.setText('Error')
         elif button.text() == '%':
             try:
                 value = float(current_text)
-                result = value / 100
-                self.display.setText(str(result))
+                self.display.setText(str(value / 100))
             except:
                 self.display.setText('Error')
         elif button.text() == '√':
             try:
                 value = float(current_text)
-                result = math.sqrt(value)
-                self.display.setText(str(result))
+                self.display.setText(str(math.sqrt(value)))
             except:
                 self.display.setText('Error')
+        elif button.text() == '⌫':
+            self.display.setText(current_text[:-1])
         else:
             self.display.setText(current_text + button.text())
 
     def show_settings_menu(self):
         menu = QMenu(self)
-        if self.dark_mode:
-            menu.setStyleSheet("QMenu { background-color: #4D4D4D; color: white; }")
-        else:
-            menu.setStyleSheet("QMenu { background-color: #E6E6E6; color: black; }")
+        menu.setStyleSheet(f"QMenu {{ background-color: {'#4D4D4D' if self.dark_mode else '#E6E6E6'}; color: {'white' if self.dark_mode else 'black'}; }}")
         dark_mode_action = menu.addAction("Toggle Dark Mode")
         dark_mode_action.triggered.connect(self.toggle_dark_mode)
+        square_buttons_action = menu.addAction("Toggle Square Buttons")
+        square_buttons_action.triggered.connect(self.toggle_square_buttons)
         about_action = menu.addAction("About")
         about_action.triggered.connect(self.show_about)
         menu.exec_(self.sender().mapToGlobal(self.sender().rect().bottomLeft()))
@@ -198,14 +175,17 @@ class Calculator(QWidget):
             if child != self.settings_button:
                 self.update_button_style(child)
 
+    def toggle_square_buttons(self):
+        self.square_buttons = not self.square_buttons
+        for child in self.findChildren(QPushButton):
+            if child != self.settings_button:
+                self.update_button_style(child)
+
     def show_about(self):
         about_box = QMessageBox(self)
         about_box.setWindowTitle("About PyCalc")
         about_box.setText("This is a simple calculator application created with PyQt5.\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.")
-        if self.dark_mode:
-            about_box.setStyleSheet("QMessageBox { background-color: #2D2D2D; color: white; }")
-        else:
-            about_box.setStyleSheet("QMessageBox { background-color: #F2F2F2; color: black; }")
+        about_box.setStyleSheet(f"QMessageBox {{ background-color: {'#2D2D2D' if self.dark_mode else '#F2F2F2'}; color: {'white' if self.dark_mode else 'black'}; }}")
         about_box.exec_()
 
 if __name__ == '__main__':
